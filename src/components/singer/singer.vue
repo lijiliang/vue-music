b8baabcd95ce02c05f94a4a898a332d4c47af23a
Benson <benson@Benson.local>
2017-07-12 17:43:25 +0800

    feat: 歌手列表数据获取并根据需求处理好数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,97 @@
 <template>
-  <div>
+  <div class="singer">
     歌手页面
   </div>
 </template>
 
-<script type="text/ecmascript-6">
+ <script type="text/ecmascript-6">
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
+
+  export default {
+    data () {
+      return {
+        singers: []
+      }
+    },
+    created () {
+      this._getSingerList()
+    },
+    methods: {
+      // 获取歌手列表
+      _getSingerList () {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this._normalizeSinger(res.data.list))
+          }
+        })
+      },
+      // 格式化数据
+      _normalizeSinger (list) {
+        // 热门数据
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+
+        // 循环获取热门数据
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+
+          // 根据字母进行重置数据
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+
+          // 循环获取字母进行数据
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+
+        // 为了得到有序列表，我们需要处理map
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        // 进行数据排序，按照A-Z的形式排列
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        // 返回数据 一维数组
+        return hot.concat(ret)
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
