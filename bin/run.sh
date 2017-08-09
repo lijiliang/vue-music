49292c4ba7b249f8ffa401f134dc4fda7376bf24
Benson <benson@Benson.local>
2017-08-09 11:09:03 +0800

    feat: 项目启动脚本

---
+++ bin/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#export PATH=/usr/local/bin:/bin:/usr/bin
+#export NODE_PATH=/usr/local/lib/node_modules
+
+pm2 flush
+pm2 start app.js -i max -n vue-music
+#pm2 start app.js -i max -n vue-music --node-args="--harmony"
+pm2 logs vue-music
